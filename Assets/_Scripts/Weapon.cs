672d8c2508a3ec61ba9710c4401d5dc76048d1e9
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-10-03 13:17:38 -0400

    Serializable WeaponDefinition Class

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//This is an enum of the possible weapon types
-//It also includes a "shield" type to allow a shield power-up.
-//Items marked [NI] beelow are Not Implemented in the IGPD Book
+///<summary
+///This is an enum of the possible weapon types
+///It also includes a "shield" type to allow a shield power-up.
+///Items marked [NI] beelow are Not Implemented in the IGPD Book
+///</summary>
 
 public enum WeaponType
 {
@@ -21,6 +23,35 @@ public enum WeaponType
     laser,      //[NI] Damage over time
     
     shield      //Raise shieldLevel
+}
+
+///<summary
+///The WeaponDefinition class alows you to set the properties
+///of a specific weapon in the Inspector. The main class has
+///an array of WeaponDefinitions that makes this possible.
+///</summary>
+
+[System.Serializable]
+
+public class WeaponDefinition
+{
+    public WeaponType type = WeaponType.none;
+
+    public string letter;
+
+    public Color color = Color.white;
+
+    public GameObject projectilePrefab;
+
+    public Color projectileColor = Color.white;
+
+    public float damageOnHit = 0;
+
+    public float continuousDamage = 0;
+
+    public float delayBetweenShots = 0;
+
+    public float velocity = 20;
 }
 
 public class Weapon: MonoBehaviour
