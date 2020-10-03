279d3e1f432d964110cc9847cc5f84cc341de974
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-10-03 13:41:23 -0400

    Generic Dictionary for WeaponDefinitions

--- Assets/_Scripts/Main.cs
+++ Assets/_Scripts/Main.cs
@@ -7,6 +7,8 @@ public class Main : MonoBehaviour
 {
     static public Main S;
 
+    static Dictionary<WeaponType, WeaponDefinition> WEAP_DICT;
+
     [Header("Set in Inspector")]
 
     public GameObject[] prefabEnemies;
@@ -66,6 +68,15 @@ public class Main : MonoBehaviour
         //Invoke SpawnEnemy() again
 
         Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+
+        //A generic Dicitonary with WeaponType as the key
+
+        WEAP_DICT = new Dictionary<WeaponType, WeaponDefinition>();
+
+        foreach(WeaponDefinition def in weaponDefinitions)
+        {
+            WEAP_DICT[def.type] = def;
+        }
     }
 
     public void DelayedRestart(float delay)
@@ -80,5 +91,31 @@ public class Main : MonoBehaviour
         //Reload _Scene_0 to restart the game
 
         SceneManager.LoadScene("_Scene_0");
+    }
+
+    ///<summary>
+    ///Static function that gets a WeaponDefinition from WEAP_DICT static
+    ///protected field of the main class.
+    /// </summary>
+    /// <returns>The WeaponDefinition or, if there is no WeaponDefinition with
+    /// the WeaponType passed in, returns as a new WeaponDefinition with a
+    /// WeaponType of none..</returns>
+    /// <param name="wt">The WeaponType of the desired WeaponDefintion</param>
+    
+    static public WeaponDefinition GetWeaponDefinition(WeaponType wt)
+    {
+        //Check to make sure that the key exists in the Dictionary
+        //Attempting to retrieve a key that didn't exist, would throw an error,
+        //so the following statement is important.
+
+        if (WEAP_DICT.ContainsKey(wt))
+        {
+            return (WEAP_DICT[wt]);
+        }
+
+        //This returns a new WeaponDefinition with a type of WeaponType.none,
+        //which means it has failed to find the right WeaponDefinition.
+
+        return (new WeaponDefinition());
     }
 }
