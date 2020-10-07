9c38221ff1198e897c05122a0e10ffe0bc07753b
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-10-07 11:46:13 -0400

    Movement of Enemy_4

--- Assets/_Scripts/Enemy_4.cs
+++ Assets/_Scripts/Enemy_4.cs
@@ -2,17 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_4 : MonoBehaviour
+/// <summary>
+/// Enemy_4 will start offscreen and then pick a random point on screen to
+/// move to. Once it has arrived, it will pick another random point and
+/// continue until the player has shot it down.
+/// </summary>
+
+public class Enemy_4 : Enemy
 {
-    // Start is called before the first frame update
+    public Vector3 p0, p1;
+
+    private float timeStart;
+
+    private float duration = 4;
+
     void Start()
     {
-        
+        //There is already an initial position chosen by Main.SpawnEnemy()
+        //So add it to points as the initial p0 & p1.
+
+        p0 = p1 = pos;
+
+        InitMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitMovement()
     {
-        
+        p0 = p1;
+
+        //Assign a new on-screen location to p1.
+
+        float widMinRad = bndCheck.camWidth - bndCheck.radius;
+
+        float hgtMidRad = bndCheck.camHeight - bndCheck.radius;
+
+        p1.x = Random.Range(-widMinRad, widMinRad);
+
+        p1.y = Random.Range(-hgtMidRad, hgtMidRad);
+
+        //Reset the time
+
+        timeStart = Time.time;
+    }
+
+    public override void Move()
+    {
+        //This completely overrides Enemy.Move() with a linear interpolation
+
+        float u = (Time.time - timeStart) / duration;
+
+        if (u >= 1)
+        {
+            InitMovement();
+
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow(1 - u, 2);
+
+        pos = (1 - u) * p0 + u * p1;
     }
 }
